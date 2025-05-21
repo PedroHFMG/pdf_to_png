d561480c2ffc0a6bcf8a7e0c90891842354fb715
PedroHFMG <pedrohborges12@gmail.com>
2025-05-21 20:00:21 -0300

    testing argparse and readme.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pdf_to_png
+# PDF to Image Converter
+Um projeto pessoal para converter páginas de arquivos PDF em imagens PNG, com suporte a diferentes linguagens e interfaces.
+
+## Objetivo
+Criar uma ferramenta simples  para extrair imagens de arquivos PDF. Será desenvolvido em múltiplas linguagens, com foco inicial em linha de comando (CLI - Command Line Interface) e, futuramente, com interface gráfica.
+
+## Suporte planejado:
+
+| Plataforma | Status        | Tipo             |
+|------------|---------------|------------------|
+| Python     | 🛠️ Em andamento | CLI            |
+| Node.js    | 🔜 Planejado | CLI               |
+| C#         | 🔜 Planejado | Interface gráfica |
+| Java       | 🔜 Planejado | Interface gráfica |
--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-import os
-import sys
-import time
-import fitz
+import os, sys, time, fitz, argparse
 
-print(time.ctime())
-print(os.getcwd())
-print(os.path.dirname(os.path.realpath(__file__)))
-print(os.path.splitext(os.path.basename(__file__))[0])
-
-def conv(path, output_dir = None, dpi=300):
-    if not os.path.exists(path):
+def conv(pdfpath, output_dir = None, dpi=300):
+    if not os.path.exists(pdfpath):
         print("Erro: o diretório do arquivo PDF não existe.")
         return False
     
     output_dir = os.path.dirname(os.path.realpath(__file__))
-    filename = os.path.splitext(os.path.basename(path))[0]
+    filename = os.path.splitext(os.path.basename(pdfpath))[0]
 
     try:
-        pdf_doc = fitz.open(path)
+        pdf_doc = fitz.open(pdfpath)
         for page in pdf_doc:
-            page_img =  page.get_pixmap()
+            page_img =  page.get_pixmap(matrix=fitz.Matrix(2, 2))
             page_img.save("page-%i.png" % page.number)
+        print("Conversão finalizada com sucesso.")
+        return True
         
     except Exception as e:
         print(f"Erro durante a conversão: {str(e)}")
         return False
     
+def main():
+    parser = argparse.ArgumentParser(description="Converte PDF em imagens.")
+    parser.add_argument('pdfpath', help='Arquivo PDF de entrada (precisa estar na mesma pasta).')
+
+    args = parser.parse_args()
+
+    conv(args.pdfpath)
+
 if __name__ == "__main__":
-    conv("test.pdf")
+    main()
